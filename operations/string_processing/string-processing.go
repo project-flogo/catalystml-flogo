9740d23d0822ceae4c2660f30f714f9569b47dac
Abram Van Der Geest <avanderg@tibco.com>
2019-09-03 14:01:17 -0400

    adding toupper and tolower

--- operations/string_processing/string-processing.go
+++ operations/string_processing/string-processing.go
@@ -7,6 +7,8 @@ import (
 	"github.com/project-flogo/cml/operations/string_processing/replace"
 	"github.com/project-flogo/cml/operations/string_processing/levenshteinDistance"
 	"github.com/project-flogo/cml/operations/string_processing/levenshteinSimilarity"
+	"github.com/project-flogo/cml/operations/string_processing/tolower"
+	"github.com/project-flogo/cml/operations/string_processing/toupper"
 	"github.com/project-flogo/cml/action/operation"
 )
 	
@@ -17,4 +19,6 @@ func init() {
 	_ = operation.Register(&replace.Operation{}, replace.New)
 	_ = operation.Register(&levenshteinDistance.Operation{}, levenshteinDistance.New)
 	_ = operation.Register(&levenshteinSimilarity.Operation{}, levenshteinSimilarity.New)
+	_ = operation.Register(&tolower.Operation{}, tolower.New)
+	_ = operation.Register(&toupper.Operation{}, toupper.New)
 }
---
+++ operations/string_processing/tolower/operation.go
@@ -0,0 +1,32 @@
+package tolower
+
+import (
+	"strings"
+
+	"github.com/project-flogo/cml/action/operation"
+	"github.com/project-flogo/core/support/log"
+)
+
+type Operation struct {
+	// params *Params
+	logger log.Logger
+}
+
+func New(ctx operation.InitContext) (operation.Operation, error) {
+
+	return &Operation{logger: ctx.Logger()}, nil
+}
+
+func (a *Operation) Eval(inputs map[string]interface{}) (interface{}, error) {
+
+	//To get the inputs in the desired types.
+	input := &Input{}
+	input.FromMap(inputs)
+
+	a.logger.Info("Executing operation tolower...", input.Str)
+
+	out := strings.ToLower(input.Str)
+	a.logger.Debug("result of tolower...", out)
+
+	return out, nil
+}
