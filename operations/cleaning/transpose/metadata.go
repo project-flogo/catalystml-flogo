d5e7860c76078dce2eb58c2645ee8c323f4426ad
SteveNY-Tibco <chuyang@tibco.com>
2019-09-09 23:02:20 -0400

    make operations use common.ToInterfaceArray

--- operations/cleaning/transpose/metadata.go
+++ operations/cleaning/transpose/metadata.go
@@ -1,8 +1,7 @@
 package transpose
 
 import (
-	"fmt"
-	"reflect"
+	"github.com/project-flogo/cml/operations/common"
 )
 
 type Input struct {
@@ -12,43 +11,7 @@ type Input struct {
 func (i *Input) FromMap(values map[string]interface{}) error {
 
 	var err error
-	i.Data, err = ToInterfaceArray(values["data"])
+	i.Data, err = common.ToInterfaceArray(values["data"])
 
 	return err
-}
-
-func ToInterfaceArray(val interface{}) ([]interface{}, error) {
-
-	switch t := val.(type) {
-	case []interface{}:
-		return t, nil
-
-	case []map[string]interface{}:
-		var a []interface{}
-		for _, v := range t {
-			a = append(a, v)
-		}
-		return a, nil
-	case nil:
-		return nil, nil
-	default:
-		s := reflect.ValueOf(val)
-		if s.Kind() == reflect.Slice {
-			a := make([]interface{}, s.Len())
-			for i := 0; i < s.Len(); i++ {
-				element := s.Index(i).Interface()
-				elementType := reflect.TypeOf(element)
-				switch elementType.Kind() {
-				case reflect.Slice:
-					a[i], _ = ToInterfaceArray(element)
-				case reflect.Array:
-					a[i], _ = ToInterfaceArray(element)
-				default:
-					a[i] = element
-				}
-			}
-			return a, nil
-		}
-		return nil, fmt.Errorf("unable to coerce %#v to []interface{}", val)
-	}
 }
--- operations/math/norm/metadata.go
+++ operations/math/norm/metadata.go
@@ -2,8 +2,9 @@ package norm
 
 import (
 	"errors"
-	"fmt"
 	"reflect"
+
+	"github.com/project-flogo/cml/operations/common"
 )
 
 type Params struct {
@@ -18,7 +19,7 @@ type Input struct {
 func (i *Input) FromMap(values map[string]interface{}) error {
 
 	var err error
-	dataArray, err := ToInterfaceArray(values["data"])
+	dataArray, err := common.ToInterfaceArray(values["data"])
 	if nil != err {
 		return err
 	}
@@ -38,40 +39,4 @@ func (i *Input) FromMap(values map[string]interface{}) error {
 	}
 
 	return err
-}
-
-func ToInterfaceArray(val interface{}) ([]interface{}, error) {
-
-	switch t := val.(type) {
-	case []interface{}:
-		return t, nil
-
-	case []map[string]interface{}:
-		var a []interface{}
-		for _, v := range t {
-			a = append(a, v)
-		}
-		return a, nil
-	case nil:
-		return nil, nil
-	default:
-		s := reflect.ValueOf(val)
-		if s.Kind() == reflect.Slice {
-			a := make([]interface{}, s.Len())
-			for i := 0; i < s.Len(); i++ {
-				element := s.Index(i).Interface()
-				elementType := reflect.TypeOf(element)
-				switch elementType.Kind() {
-				case reflect.Slice:
-					a[i], _ = ToInterfaceArray(element)
-				case reflect.Array:
-					a[i], _ = ToInterfaceArray(element)
-				default:
-					a[i] = element
-				}
-			}
-			return a, nil
-		}
-		return nil, fmt.Errorf("unable to coerce %#v to []interface{}", val)
-	}
 }
