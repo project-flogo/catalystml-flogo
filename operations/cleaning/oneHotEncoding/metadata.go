49411397737dfa14c36d5e3be49c8d09ddf81b4c
Abram Van Der Geest <avanderg@tibco.com>
2019-10-18 16:03:10 -0400

    updating oneHotEncoding

--- operations/cleaning/oneHotEncoding/metadata.go
+++ operations/cleaning/oneHotEncoding/metadata.go
@@ -1,25 +1,30 @@
 package oneHotEncoding
 
 import (
-	"github.com/project-flogo/core/data/coerce"
+	"github.com/project-flogo/catalystml-flogo/operations/common"
 )
 
 type Params struct {
-	Columns     interface{} `md:"col"`
-	SeperateOut bool        `md:"seperateOut"`
+	InputColumns  []interface{} `md:"inputColumns"`
+	OutputColumns []interface{} `md:"outputColumns",required=false`
+	KeepOrig      bool          `md:"keepOrig"`
 }
 
 type Input struct {
-	Data map[string]interface{} `md:"data"`
+	Data interface{} `md:"data"`
 }
 
 func (i *Input) FromMap(values map[string]interface{}) error {
 
-	var err error
-	i.Data, err = coerce.ToObject(values["data"])
-	if err != nil {
-		return err
+	var m map[string]interface{}
+
+	df, _ := common.ToDataFrame(values["data"])
+	m = make(map[string]interface{})
+	for k, arr := range df {
+		m[k] = arr
 	}
+
+	i.Data = m
 
 	return nil
 }
--- operations/cleaning/oneHotEncoding/operation_test.go
+++ operations/cleaning/oneHotEncoding/operation_test.go
@@ -2,23 +2,132 @@ package oneHotEncoding
 
 import (
 	"fmt"
+	"testing"
+
 	"github.com/project-flogo/catalystml-flogo/action/support/test"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func TestSample(t *testing.T) {
 	inputs := make(map[string]interface{})
 
-	inputs["data"] = map[string]interface{}{"state":[]interface{}{"CA","NC","TX"},"sample":"sampleSomething"}
-	
-	
-	params := Params{Columns: []interface{}{"state"}}
+	inputs["data"] = map[string]interface{}{"state": []interface{}{"CA", "NC", "TX"}, "sample": []interface{}{"A", "B", "C"}}
+
+	params := Params{
+		InputColumns: []interface{}{"state"},
+		KeepOrig:     false,
+	}
 
 	optInitConext := test.NewOperationInitContext(params, nil)
 	opt, err := New(optInitConext)
 	assert.Nil(t, err)
-	
+
+	result, err := opt.Eval(inputs)
+	fmt.Println("Result..", result)
+	assert.Nil(t, err)
+}
+
+func TestSampleMultColSepFalse(t *testing.T) {
+	inputs := make(map[string]interface{})
+
+	inputs["data"] = map[string]interface{}{"state": []interface{}{"CA", "NC", "TX"}, "sample": []interface{}{"A", "B", "C"}}
+
+	params := Params{
+		InputColumns: []interface{}{"state", "sample"},
+		KeepOrig:     false,
+	}
+
+	optInitConext := test.NewOperationInitContext(params, nil)
+	opt, err := New(optInitConext)
+	assert.Nil(t, err)
+
+	result, err := opt.Eval(inputs)
+	fmt.Println("Result..", result)
+	assert.Nil(t, err)
+}
+
+func TestSampleWOutput(t *testing.T) {
+	inputs := make(map[string]interface{})
+
+	inputs["data"] = map[string]interface{}{"state": []interface{}{"CA", "NY", "TX"}, "sample": []interface{}{"A", "NY", "C"}, "NY": []interface{}{1, 2, 3}}
+
+	params := Params{
+		InputColumns:  []interface{}{"state", "sample"},
+		OutputColumns: []interface{}{"CA", "NC", "TX", "NY", "A", "B", "C", "D", "E", "F"},
+		KeepOrig:      true,
+	}
+
+	optInitConext := test.NewOperationInitContext(params, nil)
+	opt, err := New(optInitConext)
+	assert.Nil(t, err)
+
+	result, err := opt.Eval(inputs)
+	fmt.Println("Result..", result)
+	assert.Nil(t, err)
+}
+
+func TestSampleMultColSepTrue(t *testing.T) {
+	inputs := make(map[string]interface{})
+
+	inputs["data"] = map[string]interface{}{"state": []interface{}{"CA", "NC", "TX"}, "sample": []interface{}{"A", "B", "C"}}
+
+	params := Params{
+		InputColumns: []interface{}{"state", "sample"},
+		KeepOrig:     true,
+	}
+
+	optInitConext := test.NewOperationInitContext(params, nil)
+	opt, err := New(optInitConext)
+	assert.Nil(t, err)
+
+	result, err := opt.Eval(inputs)
+	fmt.Println("Result..", result)
+	assert.Nil(t, err)
+}
+
+func TestSampleDefParams(t *testing.T) {
+	inputs := make(map[string]interface{})
+
+	inputs["data"] = map[string]interface{}{"state": []interface{}{"CA", "NC", "TX"}, "sample": []interface{}{"A", "B", "C"}}
+
+	params := Params{InputColumns: []interface{}{"state"}}
+
+	optInitConext := test.NewOperationInitContext(params, nil)
+	opt, err := New(optInitConext)
+	assert.Nil(t, err)
+
+	result, err := opt.Eval(inputs)
+	fmt.Println("Result..", result)
+	assert.Nil(t, err)
+}
+
+func TestSampleMtx(t *testing.T) {
+	inputs := make(map[string]interface{})
+
+	inputs["data"] = []interface{}{[]interface{}{0, 2, 4}, []interface{}{6, 8, 12}, []interface{}{14, 16, 18}}
+
+	params := Params{InputColumns: []interface{}{"0"}}
+
+	optInitConext := test.NewOperationInitContext(params, nil)
+	opt, err := New(optInitConext)
+	assert.Nil(t, err)
+
+	result, err := opt.Eval(inputs)
+	fmt.Println("Result..", result)
+	assert.Nil(t, err)
+}
+
+func TestSampleMtx2(t *testing.T) {
+	inputs := make(map[string]interface{})
+
+	inputs["data"] = []interface{}{[]interface{}{"a", "b", "c"}, []interface{}{"d", "e", "f"}, []interface{}{"g", "h", "i"}}
+
+	params := Params{InputColumns: []interface{}{"0","1","2"}}
+
+	optInitConext := test.NewOperationInitContext(params, nil)
+	opt, err := New(optInitConext)
+	assert.Nil(t, err)
+
 	result, err := opt.Eval(inputs)
 	fmt.Println("Result..", result)
 	assert.Nil(t, err)
