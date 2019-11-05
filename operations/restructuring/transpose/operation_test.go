50fd4db2041bdabb5399e6efc597233033f8d079
SteveNY-Tibco <chuyang@tibco.com>
2019-11-05 17:42:49 -0500

    Update trasnpose
    1. Using new DataFrame stuff
    2. Better printing in test
    3. documentation and descriptor

--- operations/restructuring/transpose/operation_test.go
+++ operations/restructuring/transpose/operation_test.go
@@ -7,15 +7,56 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMultipleDEval(t *testing.T) {
+func TestTable(t *testing.T) {
+
 	inputs := make(map[string]interface{})
-	inputs["data"] = [][]float32{{1.0, 2.5}, {2.0, 4.1}, {3.6, 0.9}, {4, 2.3}}
+	table := make(map[string]interface{})
+	inputs["data"] = table
+
+	table["order"] = []interface{}{
+		"col1", "col2", "col3",
+	}
+	table["col1"] = []interface{}{
+		"A", "A", "B", nil, "D", "C",
+	}
+	table["col2"] = []interface{}{
+		2, 1, 9, 8, 7, 4,
+	}
+	table["col3"] = []interface{}{
+		0, 1, 9, 4, 2, 3,
+	}
 
 	optInitConext := test.NewOperationInitContext(nil, nil)
 
 	opt, err := New(optInitConext)
 	assert.Nil(t, err)
 
-	_, err = opt.Eval(inputs)
+	out, err := opt.Eval(inputs)
 	assert.Nil(t, err)
+
+	t.Log("Input of Operation Transpose : ", table)
+	t.Log("Output of Operation Transpose : ", out)
+}
+
+func TestMatrix(t *testing.T) {
+
+	matrix := [][]interface{}{
+		{"A", "A", "B", nil, "D", "C"},
+		{2, 1, 9, 8, 7, 4},
+		{0, 1, 9, 4, 2, 0},
+	}
+
+	inputs := make(map[string]interface{})
+	inputs["data"] = matrix
+
+	optInitConext := test.NewOperationInitContext(nil, nil)
+
+	opt, err := New(optInitConext)
+	assert.Nil(t, err)
+
+	out, err := opt.Eval(inputs)
+	assert.Nil(t, err)
+
+	t.Log("Input of Operation Transpose : ", matrix)
+	t.Log("Output of Operation Transpose : ", out)
 }
