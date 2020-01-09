91fe726f41175170de9fc11143bccf99e0d6bdca
skothari-tibco <skothari@tibco.com>
2020-01-09 23:13:58 +0530

    Add simplified api

--- examples/golangWOTrigger/main.go
+++ examples/golangWOTrigger/main.go
@@ -1,28 +1,36 @@
 package main
 
 import (
-	"context"
 	"fmt"
 
 	_ "github.com/project-flogo/catalystml-flogo/action"
+	"github.com/project-flogo/catalystml-flogo/action/api"
 	_ "github.com/project-flogo/catalystml-flogo/operations/cleaning"
+	_ "github.com/project-flogo/catalystml-flogo/operations/image_processing"
 	_ "github.com/project-flogo/catalystml-flogo/operations/nlp"
 	_ "github.com/project-flogo/catalystml-flogo/operations/string_processing"
-	_ "github.com/project-flogo/catalystml-flogo/operations/image_processing"
-	"github.com/project-flogo/core/action"
 )
 
 func main() {
 
-	factory := action.GetFactory("github.com/project-flogo/catalystml-flogo/action")
+	cfg := api.CatalystmlSettings("file://cml.json")
 
-	var act action.Action
+	act, err := api.NewAction(cfg)
 
-	act, _ = factory.New(&action.Config{Settings: map[string]interface{}{"catalystMlURI": "file://cml.json"}})
+	if err != nil {
+		return
+	}
 
 	input := "Natural Language Processing (NLP) is all about leveraging tools, techniques and algorithms to process and understand natural language-based data, which is usually unstructured like text, speech and so on. In this series of articles, we will be looking at tried and tested strategies, techniques and workflows which can be leveraged by practitioners and data scientists to extract useful insights from text data. We will also cover some useful and interesting use-cases for NLP. This article will be all about processing and understanding text data with tutorials and hands-on examples."
 
-	out, _ := act.(action.SyncAction).Run(context.Background(), map[string]interface{}{"paragraph": input})
+	inputs := make(map[string]interface{})
+	inputs["paragraph"] = input
+
+	out, err := api.Run(act, inputs)
+
+	if err != nil {
+
+	}
 
 	fmt.Println("Output...", out)
 }
