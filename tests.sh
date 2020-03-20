87f26dfd6054115f1e35df5336966ab1e1018a60
Abram Van Der Geest <avanderg@tibco.com>
2020-03-20 14:47:38 -0400

    modifying tests script again and fixing an error in the tests for img2tensor

--- tests.sh
+++ tests.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+rtn=0
 cd action
 go test 
 cd ..
@@ -6,6 +7,10 @@ cd operations
 for op  in $(find . -mindepth 2 -maxdepth 2 -type d)
 do 
     cd $op
+    if ! go test; then
+        let "rtn=rtn+1"
+    fi
     go test 
     cd ../..
 done
+return $rtn
