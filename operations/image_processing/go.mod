efa86bfd8b8ca42843e2b7f39630c450dd821b38
Abram Van Der Geest <40475678+abramvandergeest@users.noreply.github.com>
2020-05-13 11:25:50 -0400

    fixing go.mods and removing go.sum (#155)
    * edit go.sum
    
    readding travis file, and removing replace statements in go.mod
    
    changing versions of go.mods to be latest version v0.1.16-hf.1
    
    pushing for travis test
    
    adding travis.yml file
    
    changing depth in travis.yml from 1 to 3
    
    adding more paths to tests
    
    adding each directory to test
    
    moving tests to a shell script that is then run in travis.yml
    
    adding sh to run bash script
    
    changing sh to bash
    
    in travis.yml changing running script from bash tests.sh to ./tests.sh
    
    modifying permissions for tests.sh
    
    removing duplicate permissions action in travis.yml
    
    adding permissions line back into travis file
    
    changing permissions of tests.sh
    
    removing permissions line in tests.sh
    
    testing over cats and ops
    
    should have a better tests.sh script now
    
    fixing issues with caps or no caps of operation directories
    
    fixing errors in running tests in action, runCML, and toLog
    
    makeing the packages in image processing refer to the current versions
    
    removing last changes to image_processing go.mod
    
    renaming subsectiontoimage to subSectionToImage
    
    fixing subSectionToImage due to travis bug
    
    changing image processing.go to camel case for all subSectionToImage
    
    undoing last change
    
    removing travis.yml
    
    updating normalize tests
    
    fixing error in test TestSingleDEvalDiff
    
    removing go.sums
    
    changing versions of go.mods to be latest version v0.1.16-hf.1
    
    pushing for travis test
    
    adding travis.yml file
    
    changing depth in travis.yml from 1 to 3
    
    adding more paths to tests
    
    adding each directory to test
    
    moving tests to a shell script that is then run in travis.yml
    
    adding sh to run bash script
    
    changing sh to bash
    
    in travis.yml changing running script from bash tests.sh to ./tests.sh
    
    modifying permissions for tests.sh
    
    removing duplicate permissions action in travis.yml
    
    adding permissions line back into travis file
    
    changing permissions of tests.sh
    
    removing permissions line in tests.sh
    
    testing over cats and ops
    
    should have a better tests.sh script now
    
    fixing issues with caps or no caps of operation directories
    
    fixing errors in running tests in action, runCML, and toLog
    
    makeing the packages in image processing refer to the current versions
    
    removing last changes to image_processing go.mod
    
    renaming subsectiontoimage to subSectionToImage
    
    fixing subSectionToImage due to travis bug
    
    changing image processing.go to camel case for all subSectionToImage
    
    undoing last change
    
    removing travis.yml
    
    updating normalize tests
    
    fixing error in test TestSingleDEvalDiff
    
    removing go.sums
    
    * getting tag right for image_processing
    
    * adding in go.sums
    
    Co-authored-by: skothari-tibco <skothari@tibco.com>

--- operations/image_processing/go.mod
+++ operations/image_processing/go.mod
@@ -6,9 +6,5 @@ require (
 	github.com/disintegration/imaging v1.6.1
 	github.com/project-flogo/catalystml-flogo/action v0.1.6
 	github.com/project-flogo/core v0.9.4
-	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.4.0
-	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
-	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- operations/math/normalize/operation_test.go
+++ operations/math/normalize/operation_test.go
@@ -1,6 +1,7 @@
 package normalize
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/project-flogo/core/data/coerce"
@@ -19,15 +20,20 @@ func TestSingleDEval(t *testing.T) {
 
 	//input := &Input{Array: []float32{1.0, 2.0, 3.6, 4}}
 
-	_, err = opt.Eval(inputs)
-
+	out, err := opt.Eval(inputs)
+	fmt.Println(out)
+	var test []float32
+	for _, val := range out.([]interface{}) {
+		test = append(test, val.(float32))
+	}
 	assert.Nil(t, err)
+	assert.Equal(t, []float32{-0.055555556, 0.055555556, 2.3888888, 0.16666667}, test)
 
 }
 func TestSingleDEvalDiff(t *testing.T) {
 	//params := Params{}
 	opt := &Operation{logger: log.RootLogger()}
-	val, err := coerce.ToArray([]interface{}{"Age", 1, 3, 45, 5})
+	val, err := coerce.ToArray([]float32{1, 3, 45, 5})
 	inputs := make(map[string]interface{})
 	inputs["data"] = val
 	inputs["value"] = 20
@@ -35,9 +41,14 @@ func TestSingleDEvalDiff(t *testing.T) {
 
 	//input := &Input{Array: []float32{1.0, 2.0, 3.6, 4}}
 
-	_, err = opt.Eval(inputs)
-
+	out, err := opt.Eval(inputs)
+	fmt.Println(out)
+	var test []float32
+	for _, val := range out.([]interface{}) {
+		test = append(test, val.(float32))
+	}
 	assert.Nil(t, err)
+	assert.Equal(t, []float32{-0.055555556, 0.055555556, 2.3888888, 0.16666667}, test)
 
 }
 func TestMultiple2DEval(t *testing.T) {
@@ -48,7 +59,8 @@ func TestMultiple2DEval(t *testing.T) {
 	inputs["value"] = 20
 	inputs["minvalue"] = 2
 
-	_, err = opt.Eval(inputs)
+	out, err := opt.Eval(inputs)
+	fmt.Println(out)
 
 	assert.Nil(t, err)
 
